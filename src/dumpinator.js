7ccd84c3718b3b77644c6f168a8a95b5e40e457f
Andifeind <andifeind@noname-media.com>
2016-11-22 11:57:48 +0100

    SDK class

---
+++ src/dumpinator.js
@@ -0,0 +1,23 @@
+const co = require('co');
+const config = {};
+
+class Dumpinator {
+  static run() {
+    const tests = config.tests;
+
+    return co(function *() {
+      for (const test in tests || []) {
+        const res = yield this.runOne(test);
+
+      }
+    });
+  }
+
+  runOne(test) {
+    return co(function *() {
+      
+    });
+  }
+}
+
+module.exports = Dumpinator;
