198e7d4bb2663f236b457447d39a7df433380e6c
Christian Neuhäuser <chris@type10.com>
2016-12-02 10:14:35 +0100

    Add dumpinator config files for testing purposes, will be removed again

---
+++ dumpinator.conf.js
@@ -0,0 +1,20 @@
+'use strict';
+
+module.exports = {
+  defaults: {
+    left: {
+      hostname: 'http://a.b/'
+    },
+    right: {
+      hostname: 'http://x.y/'
+    }
+  },
+  routes: [
+    {
+      url: '/jsconfig1'
+    },
+    {
+      url: '/jsconfig2'
+    }
+  ]
+};
