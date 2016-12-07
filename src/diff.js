4f179220b6873bc0ff9c1a930dcad376be56f4b4
Andifeind <andifeind@noname-media.com>
2016-12-07 14:31:57 +0100

    Diff module

---
+++ src/diff.js
@@ -0,0 +1,42 @@
+'use strict';
+
+const jsdiff = require('diff');
+const sortify = require('json.sortify');
+
+class Diff {
+  diff(left, right) {
+    return new Promise((resolve, reject) => {
+      let diffResult;
+
+      if (typeof left !== typeof right) {
+        throw new Error('Can not compare tow different source types!');
+      }
+
+      if (typeof left === 'object') {
+        diffResult = jsdiff.diffLines(sortify(left, null, '  '), sortify(right, null, '  '));
+      } else {
+        diffResult = jsdiff.diffLines(left, right);
+      }
+
+      resolve(diffResult);
+    });
+  }
+
+  compare(left, right) {
+    if (typeof left !== typeof right) {
+      throw new Error('Can not compare tow different source types!');
+    }
+
+    let leftStr = left;
+    let rightStr = right;
+
+    if (typeof left === 'object') {
+      leftStr = sortify(left);
+      rightStr = sortify(right);
+    }
+
+    return leftStr === rightStr;
+  }
+}
+
+module.exports = Diff;
