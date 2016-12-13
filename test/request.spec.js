375d4ace6b2825652e25d46ff522937cccf6df25
Andifeind <andifeind@noname-media.com>
2016-12-13 17:28:32 +0100

    return promise in a test

--- test/request.spec.js
+++ test/request.spec.js
@@ -8,18 +8,17 @@ inspect.useSinon(sinon);
 
 describe('Request', () => {
   describe('load()', () => {
-    it('returns a correct output', function (done) {
+    it('returns a correct output', function () {
       this.timeout(5000);
       const req = new Request();
       const options = {
         url: 'https://api.github.com/users/maxdome/repos'
       };
 
-      req.load(options)
+      return req.load(options)
         .then((res) => {
           inspect(res).isObject();
           inspect(res).hasKeys(['headers', 'body']);
-          done();
         });
     });
   });
