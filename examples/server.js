dbe31b8d85cbea9e387a7a9afe9914fd4e678ad4
Andifeind <andifeind@noname-media.com>
2016-12-13 16:01:11 +0100

    fixed linter issues

--- examples/server.js
+++ examples/server.js
@@ -7,4 +7,4 @@ const app = express();
 app.use(express.static(path.join(__dirname, '../test/fixtures/')));
 app.listen(3333, '127.0.0.1');
 
-console.log('Server listen at port 3333');
+console.log('Server listen at port 3333'); // eslint-disable-line
