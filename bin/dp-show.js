65f1ef3987dd18755f2115166a9804af7d9a10a7
Christian Neuhäuser <chris@type10.com>
2016-12-21 15:47:53 +0100

    CLI rework

---
+++ bin/dp-show.js
@@ -0,0 +1,31 @@
+#!/usr/bin/env node
+
+'use strict';
+
+const program = require('commander');
+
+const Dumpinator = require('../src/dumpinator');
+const CLIUtils = require('../src/utils/cli-utils');
+
+program
+  .option('-C, --no-color', 'disable cli colors')
+  .option('-d, --debug', 'enable debug mode')
+  .option('-F, --full', 'show the full diff')
+  .option('-v, --verbose', 'be more verbose');
+
+program
+  .command('<id>', 'show a diff of the given id')
+  .action((id, options) => {
+    options = options || {};
+
+    Dumpinator.diff(id).then((diff) => {
+      Dumpinator.reportDiff(diff, {
+        showFullDiff: !!options.full,
+        noColor: !!options.noColor
+      });
+    }).catch((err) => {
+      CLIUtils.generalErrorHandler(err);
+    });
+  });
+
+program.parse(process.argv);
